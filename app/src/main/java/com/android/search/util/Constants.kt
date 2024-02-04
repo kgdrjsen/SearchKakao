49d3c987619cf82e8bebfb3b19ede2aa04f327ac
kgdrjsen <gundoo3333@gmail.com>
2024-02-04 13:09:53 +0900

    add : Util

---
+++ app/src/main/java/com/android/search/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.android.search.util
+
+object Constants {
+    val BASE_URL = "https://dapi.kakao.com"
+    val HEADER = "KakaoAK 2a9592d47e0656c9dcfa6cb012cebc68"
+    var SEARCH_TYPE_IMAGE = 0
+    var SEARCH_TYPE_VIDEO = 1
+}
